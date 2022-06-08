6b3bd8b78cf543d93a3787a326c4cb05167c3567
KyuroTM <hazenberg.ryan@gmail.com>
2022-06-08 15:56:53 +0200

    w

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hailstone|Home</title>
+</head>
+<body>
+<?php
+    include_once ("Template_Header.php")
+?>
+
+<?php
+include_once ("Template_Footer.php")
+?>
+</body>
+</html>
