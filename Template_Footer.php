40e35787b89a63738307b29dcb8ea39efb3df20e
Rekan <3352de@gmail.com>
2022-06-09 01:52:26 +0200

    asiosfe

--- Template_Footer.php
+++ Template_Footer.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="Code/CSS/Footer.css">
+<link rel="stylesheet" href="footer.css">
 <Footer>
     <h5>All copyright reserver to &copy;Hailstone - 2022</h5>
     <img src="Media/Logo/Hailstone-Logo.png" alt="Hailstone-Logo">
