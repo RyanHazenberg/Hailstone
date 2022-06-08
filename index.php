6297bf4b4e059136bd2e3b753789d0e9761e991f
Rekan <3352de@gmail.com>
2022-06-08 22:56:02 +0200

    Updatee

--- index.php
+++ index.php
@@ -11,12 +11,15 @@
     <link rel="stylesheet" href="Code/CSS/Index.css">
     <link rel="icon" type="image/x-icon" href="Media/Logo/favicon.ico">
     <title>Home | Hailstone</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="header.css">
 </head>
 
 <body>
-    <!-- <?php
+    <!--
     include_once ("Template_Header.php")
-?> -->
+    -->
     <div class="container">
         <div class="Title">
             <h1 id="Title">About us<img src="Media/Logo/Hailstone-logo.png" alt="Hailstone Logo" id="About-Logo"></h1>
