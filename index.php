3ac85620c85716521cbd69b96e00402bffec0274
KyuroTM <hazenberg.ryan@gmail.com>
2022-06-09 16:28:49 +0200

    w

--- index.php
+++ index.php
@@ -13,8 +13,6 @@
 
     <link rel="icon" type="image/x-icon" href="Media/Logo/favicon.ico">
     <title>Home | Hailstone</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 
 <body>
