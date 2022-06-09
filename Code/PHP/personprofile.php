8a0ffe59528d178f9b64392ae303d83b0bcdc041
KyuroTM <hazenberg.ryan@gmail.com>
2022-06-09 12:04:52 +0200

    re-organized

--- Code/PHP/personprofile.php
+++ Code/PHP/personprofile.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="personsprofile.css" rel="stylesheet">
+    <link href="../CSS/personsprofile.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
