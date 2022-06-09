c8b7d68d660786d95f363e510b029caa68c93231
Rekan <3352de@gmail.com>
2022-06-09 16:33:07 +0200

    gbetrahn

--- Template_Header.php
+++ Template_Header.php
@@ -1,23 +1,25 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<link rel="stylesheet" href="Code/CSS/header.css">
-
-  <header>
-    <img class="imgheader" src="Media/Logo/Hailstone-Logo.png" alt="logo">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>RekanSalehWebsite</title>
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
     <div id="header1">
       <a class="button Hbutton" href="#" role="button">Home</a>
-      <a class="button Hbutton" href="#" role="button">Profile</a>
-      <a class="button Hbutton" href="#" role="button">Dashboard</a>
+      <a class="button Hbutton" href="#" role="button">Work</a>
+      <a class="button Hbutton" href="#" role="button">About me</a>
     </div>
     <div id="header2">
-      <a class="button Hbutton" href="#" role="button">Profile</a>
-      <a class="button Hbutton meme" href="#" role="button">Login</a>
+      <a class="button Hbutton" href="#" role="button">Contact</a>
     </div>
-<<<<<<< HEAD
-=======
-  </header>
->>>>>>> 09643b4ef0b7e5eabc057be9d8908a72114e82ff
-
   </header>
 </body>
 </html>
-
--- personprofile.php
+++ personprofile.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,58 +9,67 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="personsprofile.css" rel="stylesheet">
+    <link href="profile.css" rel="stylesheet">
 </head>
+
 <body>
-<div class="container">
-<div class="row">
-    <div class="col-md-4 mx-auto">
-        <div class="row mx-auto">
-            <div class="col-md-12 mx-auto">
-                <img src="https://via.placeholder.com/150" class="rounded-circle" alt="Cinque Terre" width="250" height="250">
+    <?php
+    include_once("Template_Header.php");
+    ?>
+    <div class="container">
+        <div class="row" id="inner">
+            <div class="col-md-4">
+                <div class="row">
+                    <div class="col-md-12" id="placeholder">
+                        <img src="https://via.placeholder.com/150" class="rounded-circle" alt="Cinque Terre" width="250"
+                            height="250">
+                    </div>
+                    <div class="col-md-12" id="placeholder2">
+
+                        <div id="profileButtonDiv">
+                        <h2>Profile Name</h2>
+                        <p>Profession</p>
+                        <p>Links</p>
+                        <p>Skills</p>
+                            <button class="profileButton">Edit Profile</button>
+                            <button class="profileButton">Contact</button>
+                            <button class="profileButton">Download Resume</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-12">
-                <h2>Profile Name</h2>
-                <p>Profession</p>
-                <p>Links</p>
-                <p>Skills</p>
-                <button class="profileButton">Edit Profile</button>
-                <button class="profileButton">Contact</button>
-                <button class="profileButton">Download Resume</button>
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                Basic card
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                Basic card
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                Basic card
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
+
+
+
     </div>
-    <div class="col-md-8">
-        <div class="row">
-           <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                    Basic card
-                    </div>
-                </div>
-           </div>
-           <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                    Basic card
-                    </div>
-                </div>
-           </div>
-           <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                    Basic card
-                    </div>
-                </div>
-           </div>  
-        </div>   
-        
-    </div>
-</div>
-
-
-
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
