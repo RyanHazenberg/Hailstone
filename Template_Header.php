5c490256d3eefae1768fdf3241f7af88c5f693b6
KyuroTM <hazenberg.ryan@gmail.com>
2022-06-09 02:22:06 +0200

    w

--- Template_Header.php
+++ Template_Header.php
@@ -1,4 +1,6 @@
 <link rel="stylesheet" href="Code/CSS/Header.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <header>
     <img src="/Media/Logo/Hailstone-Logo.png" alt="logo">
     <div id="header1">
@@ -10,7 +12,7 @@
       <a class="button Hbutton" href="#" role="button">Profile</a>
       <a class="button Hbutton meme" href="#" role="button">Login</a>
     </div>
-    
+
   </header>
 
 
