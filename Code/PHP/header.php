54a2a9599a2c0aadd3f84285e7523abc9637e850
Rekan <3352de@gmail.com>
2022-06-10 03:14:24 +0200

    weferfew

---
+++ Code/PHP/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>RekanSalehWebsite</title>
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="../css/header.css">
+</head>
+
+<body>
+
+  <header>
+    <img src="../../Media/Logo/Hailstone-Logo.png" class="imgheader" alt="logo">
+    <div id="header1">
+      <a class="button Hbutton" href="#" role="button">Home</a>
+      <a class="button Hbutton" href="#" role="button">Dashboard</a>
+      <a class="button Hbutton" href="#" role="button">About me</a>
+    </div>
+    <div id="header2">
+      <a class="button Hbutton" href="#" role="button">Profile</a>
+      <a class="button Hbutton mememe" href="#" role="button">Log out</a>
+    </div>
+
+  </header>
+
+  </body>
+
+</html>
